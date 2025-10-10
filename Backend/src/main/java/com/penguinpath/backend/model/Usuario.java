709a1958e5887aff4cad073b21a65ed185a703e4
Juanis <juanita.camca@gmail.com>
2025-10-09 19:52:17 -0500

    Fix merge conflicts in pom.xml and Usuario.java

--- Backend/src/main/java/com/penguinpath/backend/model/Usuario.java
+++ Backend/src/main/java/com/penguinpath/backend/model/Usuario.java
@@ -19,11 +19,7 @@ public class Usuario {
     @Column(name = "correo", nullable = false, length = 60)
     private String correo;
 
-<<<<<<< HEAD
-    @Column(name = "password", nullable = false, length = 100)
-=======
     @Column(name = "`password`", nullable = false, length = 100)
->>>>>>> 284a2c820f856cef3f98c81a13586cc72dd7ec9d
     private String password;
 
     @ColumnDefault("1")
