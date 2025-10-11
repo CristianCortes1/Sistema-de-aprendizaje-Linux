a30d7b69330b91dcaa373bf86c239459dbcce3b3
Cristian Cortes <bejaranno05@gmail.com>
2025-10-10 22:38:28 -0500

    Alter column type for "contraseña" in "Usuarios" table to VARCHAR(200)

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+    imports: [AuthModule],
+    controllers: [],
+    providers: [PrismaService],
 
 })
-export class AppModule {}
+export class AppModule { }
