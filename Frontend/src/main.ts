51319731255aab9b71b4cefc363a36eb55732c3b
Cristian Cortes <bejaranno05@gmail.com>
2025-11-12 21:57:14 -0500

    Force rebuild frontend - add build version comment

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/'
 
+// Build version: 2025-11-13
 const app = createApp(App)
 
-app.use(router)  // 👈 importante
+app.use(router)
 app.mount('#app')
