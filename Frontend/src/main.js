981f5b05f7e009767ba46175091875a8ed0b5ffb
Cristian Cortes <bejaranno05@gmail.com>
2025-11-12 22:22:09 -0500

    Clean up: remove all compiled .js files and improve .gitignore/.dockerignore

--- Frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router/';
-// Build version: 2025-11-13
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
