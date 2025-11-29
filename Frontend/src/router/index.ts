d5431148e3e91f134a5a7bbc8d638b8928041e3d
Cristian Cortes <bejaranno05@gmail.com>
2025-11-29 00:52:35 -0500

    feat: implement initial Home page component with UI and configure its routing.

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import AuthService from '../services/AuthService';
+import Home from '@/components/Home.vue';
 import Login from '@/components/Login.vue';
 import Dashboard from '@/components/Dashboard.vue';
 import Registro from '@/components/Registro.vue';
@@ -17,6 +18,11 @@ import ResetPassword from '../components/ResetPassword.vue';
 const routes = [
     {
         path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/login',
         name: 'Login',
         component: Login,
         meta: { guestOnly: true }
